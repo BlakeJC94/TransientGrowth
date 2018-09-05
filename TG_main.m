da7338dae17789ece3369f37791c8ed3d36ee635
Blake <blakejamescook@gmail.com>
2018-09-05 15:03:39 +1000

    Finished basic sweeper plot
    TODO:
    - rejig input handles for code stratification
    - write new tests

--- TG_main.m
+++ TG_main.m
@@ -217,6 +217,35 @@ else
     end
 
     % TODO: plots??
+    G_max_avg = mean(sweep_results(:,:,1), 2);
+    G_max_std = std(sweep_results(:,:,1), 0, 2);
+
+    t_opt_avg = mean(sweep_results(:,:,2), 2);
+    t_opt_std = std(sweep_results(:,:,2), 0, 2);
+    
+    G_init_slope_avg = mean(sweep_results(:,:,3), 2);
+    G_init_slope_std = std(sweep_results(:,:,3), 0, 2);
+
+    figure;
+    errorbar(var_vec,G_max_avg,G_max_std,'ko-'); 
+    xlim([min(var_vec)-0.1*range(var_vec), max(var_vec)+0.1*range(var_vec)]);
+    xlabel(var_str);
+    title(['G\_max average over ' num2str(total_runs) ' runs']);
+    plot_export_fig(-1, [out_dir 'figures/g_max_avg'], 14, 7/5, 18);
+    
+    figure;
+    errorbar(var_vec,t_opt_avg,t_opt_std,'ko-');
+    xlim([min(var_vec)-0.1*range(var_vec), max(var_vec)+0.1*range(var_vec)]);
+    xlabel(var_str);
+    title(['t\_opt average over ' num2str(total_runs) ' runs']);
+    plot_export_fig(-1, [out_dir 'figures/t_opt_avg'], 14, 7/5, 18);
+
+    figure;
+    errorbar(var_vec,G_init_slope_avg,G_init_slope_std,'ko-');        
+    xlim([min(var_vec)-0.1*range(var_vec), max(var_vec)+0.1*range(var_vec)]);
+    xlabel(var_str);
+    title(['G\_init\_slope average over ' num2str(total_runs) ' runs']);
+    plot_export_fig(-1, [out_dir 'figures/G_init_slope_avg'], 14, 7/5, 18);
 
 
     % Save data to dir
